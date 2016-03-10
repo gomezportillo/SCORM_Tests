b23f03149411968cd122cf3567a8891d481cb860
pedroma-gomezp <pedromanuel.gomezportillo@alu.uclm.es>
2016-03-10 18:06:07 +0100

    implemented toLatex and toHtml. slight changes on main.py

--- in/test3.md
+++ in/test3.md
@@ -2,7 +2,7 @@
 ::¿Son estas respuestas correctas?
 =Esta es correcta
 ~Esta no lo es
-::Y qué me dice de estas?
+::¿Y qué me dice de estas?
 ~Esta es falsa
 =Esta es la correcta
 [testend]
--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ class Question:
         self.answers = []
         self.correct = None
 
+
 class Test:
     def __init__(self, title):
         self.title = title
@@ -20,27 +21,57 @@ class Test:
         self.alphabet = list(ascii_lowercase) #letters from a to z
 
     def __repr__(self):
-        test_txt = self.title
+        test_repr = self.title
 
-        for question in self.questions:    #on each question
-            test_txt += question.statement
+        for question in self.questions:     #on each question
+            test_rep += question.statement
 
-            for answer in question.answers:     #on each answer
-                test_txt += answer
+            for answer in question.answers: #on each answer
+                test_repr += answer
 
-        return test_txt
+        return test_repr
+
+    def toHtml(self):
+
+        separator='\n'
+
+        test_txt = '{}{}{}'.format('<p>', self.title, '</p>')
+        test_txt += '<blockquote>'
+        for index_q, question in enumerate(self.questions):
+            test_txt += '{}. {}{}'.format(index_q+1, question.statement, separator)
+            test_txt += '\n<blockquote>'
+
+            for index_a, answer in enumerate(question.answers):
+                tmp_answer = '{}({}) {}{}'.format('<p>', self.alphabet[index_a], answer[1:], '</p>')
+                test_txt += tmp_answer
+            test_txt += '\n</blockquote>'
+
+        test_txt += '</blockquote>'
+        return test_txt + separator
 
     def toLatex(self):
-        test_txt = 'LaTeX - ' + self.title + '\n'
 
-        for question in self.questions:
-            test_txt += question.statement[2:]
+        separator = '\\\\'
+        tabulator = '\\hspace*{10mm}'
 
-            for index, answer in enumerate(question.answers):
-                tmp_answer = '\t('+self.alphabet[index]+') ' + answer[1:]
+        test_txt = '{}{}{}'.format(self.title, self.getCorrectAnswers(), separator)
+
+        for index_q, question in enumerate(self.questions):
+            test_txt += '{}. {}{}'.format(index_q+1, question.statement, separator)
+
+            for index_a, answer in enumerate(question.answers):
+                tmp_answer = '{}({}) {}{}'.format(tabulator, self.alphabet[index_a], answer[1:], separator)
                 test_txt += tmp_answer
 
         return test_txt
+
+    def getCorrectAnswers(self):
+        test_results = 'Los resultados del test del capítulo {} son '.format(self.chapter)
+
+        for index_q, question in enumerate(self.questions):
+            test_results += '{}({}), '.format(index_q+1, self.alphabet[question.correct])
+
+        return '\\footnote{{{}.}}'.format(test_results[:-2])
 
 def generate_test():
     #Reading markdown test file, generating and writting down html scorm test file
@@ -75,8 +106,9 @@ def generate_test():
                 #raise InvalidScormTestFormatException
                 pass
 
-    print mytest.toLatex()
+    #print mytest.toLatex()
     output_file_name = 'out/output_test_.html'
+    output_file_txt = ''
     with codecs.open(output_file_name, 'w+', encoding='utf8') as f:
 
         #HEADER
@@ -91,6 +123,7 @@ def generate_test():
             tmp_header += "var question{0};\n".format(i)
             tmp_header += "var key{0} = {1};\n".format(i, (mytest.questions[i].correct)+1 )
         f.write(tmp_header)
+        output_file_txt += tmp_header
 
         #GETANSWER
         tmp_getAnswer = codecs.open(os.path.join(templateDir, 'scorm_test_getAnswer.hit'), encoding='utf-8').read()
@@ -106,6 +139,7 @@ def generate_test():
             tmp_getAnswer += tmp_answer_getAnswer
 
         f.write(tmp_getAnswer + '}\n')
+        output_file_txt += tmp_getAnswer + '}\n'
 
         #CALCRAWSCORE
         tmp_calcRawScore = codecs.open(os.path.join(templateDir, 'scorm_test_calcRawScore.hit'), encoding='utf-8').read()
@@ -117,17 +151,20 @@ def generate_test():
             tmp_calcRawScore +=  answer_calcRawScore_templ.substitute(num_iteration=i)
 
         f.write(tmp_calcRawScore + '}\n')
+        output_file_txt += tmp_calcRawScore + '}\n'
 
         #CALCSCORE
         calcScore = codecs.open(os.path.join(templateDir, 'scorm_test_calcScore.hit'), encoding='utf-8')
         calcScore_file = calcScore.read()
         f.write(calcScore_file)
+        output_file_txt += calcScore_file
 
         #TEST HEADER
         test_header_file = codecs.open(os.path.join(templateDir, 'scorm_test_test_header.hit'), encoding='utf-8')
         test_header_templ = Template(test_header_file.read())
         test_header = test_header_templ.substitute(Title=mytest.title)
         f.write(test_header)
+        output_file_txt += test_header
 
         #PREGUNTAS Y RESPUESTAS
         tmp_pregyresp = ''
@@ -155,12 +192,13 @@ def generate_test():
         tmp_pregyresp += '</div>\n'
 
         f.write(tmp_pregyresp)
+        output_file_txt += tmp_pregyresp
 
         #FOOTER
         test_footer_file = codecs.open(os.path.join(templateDir, 'scorm_test_footer.hit'), encoding='utf-8')
         test_footer = test_footer_file.read()
         f.write(test_footer)
-
+        output_file_txt += test_footer
 
 generate_test()
 
